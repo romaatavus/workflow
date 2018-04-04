7cde929adff8bcf65b08b429c69d6f2f6c4f6a75
Lucius Brutus <romaatavus@mail.ru>
2018-04-04 12:11:29 +0300

    dev: added doctrine2 snippet

---
+++ snippets/doctrine.php
@@ -0,0 +1,19 @@
+<?php
+
+// Fix: Include Composer Autoload
+require_once "vendor/autoload.php";
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+// Fix: Include Path Database
+$paths = array("/path/to/entity-files");
+$dbParams = array(
+    'driver' => 'pdo-pgsql',
+    'user' => 'support',
+    'password' => 'workflow',
+    'dbname' => 'root',
+);
+
+$config = Setup::createAnnotationMetadataConfiguration($paths);
+$entityManager = EntityManager::create($dbParams, $config);
