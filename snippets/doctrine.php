f40546f0d80de224cba7e74f7125567da05a5dc9
Lucius Brutus <romaatavus@mail.ru>
2018-04-04 13:29:57 +0300

    fix: edited doctrine2 driver

--- snippets/doctrine.php
+++ snippets/doctrine.php
@@ -1,15 +1,15 @@
 <?php
 
 // Fix: Include Composer Autoload
-require_once "vendor/autoload.php";
+require_once "../vendor/autoload.php";
 
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
 // Fix: Include Path Database
-$paths = array("/path/to/entity-files");
+$paths = array("../path/to/entity-files");
 $dbParams = array(
-    'driver' => 'pdo-pgsql',
+    'driver' => 'pdo_pgsql',
     'user' => 'support',
     'password' => 'workflow',
     'dbname' => 'root',
